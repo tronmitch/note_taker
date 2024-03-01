ceeec5f9c3fc11d4dfcbc33d2817c5eee014b070
tronmitch <theron.mitchell@gmail.com>
2024-03-01 15:54:08 -0700

    changed or from | to ||

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs/promises');
 const path = require('path')
 
-const PORT = process.env.PORT | 3001;
+const PORT = process.env.PORT || 3001;
 
 const noteData = require('./db/db.json');
 
